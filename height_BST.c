0a57f3975c2e14e3a73d6a58f079dfc00ef8870c
Lochanan <arv.lochanan@gmail.com>
2019-05-14 14:47:03 +0530

    Add files via upload

---
+++ height_BST.c
@@ -0,0 +1,100 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#define max(a,b) (a>b?a:b)
+
+struct Node 
+{
+	int data; 
+	struct Node* left;
+	struct Node* right;
+};
+
+struct Node* GetNewNode(int data) 
+{
+	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+	newNode->data = data;
+	newNode->left = newNode->right = NULL;
+	return newNode;
+}
+
+struct Node* Insert(struct Node* root,int data) 
+{
+	if(root == NULL)
+	{ 
+		root = GetNewNode(data);
+	}
+	 
+	else if(data <= root->data) 
+	{
+		root->left = Insert(root->left,data);
+	}
+	 
+	else 
+	{
+		root->right = Insert(root->right,data);
+	}
+	return root;
+}
+
+bool Search(struct Node* root,int data) 
+{
+	if(root == NULL) 
+	{
+		return false;
+	}
+	else if(root->data == data) 
+	{
+		return true;
+	}
+	else if(data <= root->data) 
+	{
+		return Search(root->left,data);
+	}
+	else 
+	{
+		return Search(root->right,data);
+	}
+}
+int FindHeight(struct Node *root)
+{
+	if(root==NULL)
+	{
+		return -1;
+	}
+	return max(FindHeight(root->left),FindHeight(root->right))+1;
+}
+int main() 
+{
+	struct Node* root = NULL;
+	int n;
+	printf("Please enter the number of elements you want to enter in the BST: \n");
+	scanf("%d", &n);
+	for(int i=0; i<n; i++)
+	{
+		int data;
+		printf("Please Enter the element to be addeed in your BST: ");
+		scanf("%d",&data);
+		root = Insert(root,data);
+		printf("\n");
+	}	
+		/*root = Insert(root,10);	
+		root = Insert(root,20);
+		root = Insert(root,25);
+		root = Insert(root,8);
+		root = Insert(root,12);*/
+	int number;
+	printf("Enter number be searched\n");
+	scanf("%d", &number);
+	if(Search(root,number) == true)
+	{
+		printf("Found\n");
+	}
+	else
+	{
+		 printf("Not Found\n");
+	}
+	int height = FindHeight(root);
+	printf("Height of the tree is : %d", height);
+	printf("\n");
+}
